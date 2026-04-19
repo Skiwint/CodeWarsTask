6ad3be085bfa2063cc056a6484c952e4a11910a1
Skiwint <abakarov2512@gmail.com>
2026-04-19 23:18:11 +0300

    fff

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,6 +9,8 @@ public class Main {
     public static void main(String[] args) {
 
 
+
+
                 for(int i = 0; i <= 1000; i++){
             if(i % 3 == 0 && i % 5 != 0){
                 String test = String.valueOf(i);
