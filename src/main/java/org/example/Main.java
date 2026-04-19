c05d9a585a711284d2068a80866e9804c7f14efb
Skiwint <abakarov2512@gmail.com>
2026-04-19 23:24:51 +0300

    commit with error

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,7 +11,7 @@ public class Main {
 
 
 
-                for(int i = 0; i <= 1000; i++){
+        ..  for(int i = 0; i <= 1000; i++){
             if(i % 3 == 0 && i % 5 != 0){
                 String test = String.valueOf(i);
                 char[] Arrval = test.toCharArray();
